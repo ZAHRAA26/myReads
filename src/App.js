c5104cde5ede01ede70d4082224169c1c38870ea
ZAHRAA26 <zahraa.elsayed26@yahoo.com>
2026-04-22 08:48:08 +0200

    edit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import { Route, Switch } from "react-router";
 import { Link } from "react-router-dom";
@@ -6,136 +6,130 @@ import * as BooksAPI from "./BooksAPI";
 import BookShelf from "./components/BookShelf";
 import Search from "./components/Search";
 
-// import { object } from "prop-types";
-// import * as BooksAPI from "./BooksAPI";
-class BooksApp extends React.Component {
-  _isMounted = false;
-  state = {
-    query: "",
-    currentlyReading: [],
-    read: [],
-    wantToRead: [],
-  };
+function BooksApp() {
+  const [currentlyReading, setCurrentlyReading] = useState([]);
+  const [wantToRead, setWantToRead] = useState([]);
+  const [read, setRead] = useState([]);
 
-  async componentDidMount() {
-    this._isMounted = true;
-    const books = await BooksAPI.getAll();
+  // search
+  const [searchTerm, setSearchTerm] = useState("");
+  const [debouncedSearchTerm, setDebouncedSearchTerm] = useState("");
 
-    const currentlyReading = books.filter(
-      (book) => book.shelf === "currentlyReading"
-    );
-    const wantToRead = books.filter((book) => book.shelf === "wantToRead");
-    const read = books.filter((book) => book.shelf === "read");
-    this.setState({ currentlyReading, wantToRead, read });
-  }
+  // debounce
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setDebouncedSearchTerm(searchTerm);
+    }, 500);
 
-  removeBook = (book) => {
-    const bookShelf = book.shelf;
-    const stateObject = Object.assign({}, this.state);
-    for (const [key, value] of Object.entries(stateObject)) {
-      if (key === bookShelf) {
-        for (const v of value) {
-          if (v.id === book.id) {
-            value.splice(value.indexOf(v, 0), 1);
-          }
-        }
-      }
-      this.setState({
-        ...stateObject,
-      });
+    return () => clearTimeout(timer);
+  }, [searchTerm]);
+
+  // تحميل الكتب
+  useEffect(() => {
+    BooksAPI.getAll().then((books) => {
+      setCurrentlyReading(
+        books.filter((book) => book.shelf === "currentlyReading")
+      );
+      setWantToRead(
+        books.filter((book) => book.shelf === "wantToRead")
+      );
+      setRead(
+        books.filter((book) => book.shelf === "read")
+      );
+    });
+  }, []);
+
+  const removeBook = (book) => {
+    if (book.shelf === "currentlyReading") {
+      setCurrentlyReading((prev) =>
+        prev.filter((b) => b.id !== book.id)
+      );
+    }
+    if (book.shelf === "wantToRead") {
+      setWantToRead((prev) =>
+        prev.filter((b) => b.id !== book.id)
+      );
+    }
+    if (book.shelf === "read") {
+      setRead((prev) =>
+        prev.filter((b) => b.id !== book.id)
+      );
     }
   };
 
-  changeShelf = (book, newShelf) => {
+  const changeShelf = (book, newShelf) => {
     BooksAPI.update(book, newShelf).then(() => {
-      this.removeBook(book);
+      removeBook(book);
       book.shelf = newShelf;
-      switch (newShelf) {
-        case "none": {
-          break;
-        }
-        case "currentlyReading": {
-          this.setState({
-            currentlyReading: this.state.currentlyReading
-              .filter((oldbook) => oldbook.id !== book.id)
-              .concat(book),
-          });
-          break;
-        }
-        case "wantToRead": {
-          this.setState({
-            wantToRead: this.state.wantToRead
-              .filter((oldbook) => oldbook.id !== book.id)
-              .concat(book),
-          });
-          break;
-        }
-        case "read": {
-          this.setState({
-            read: this.state.read
-              .filter((oldbook) => oldbook.id !== book.id)
-              .concat(book),
-          });
-          break;
-        }
-        default:
-          return this.state;
+
+      if (newShelf === "currentlyReading") {
+        setCurrentlyReading((prev) => [...prev, book]);
+      } else if (newShelf === "wantToRead") {
+        setWantToRead((prev) => [...prev, book]);
+      } else if (newShelf === "read") {
+        setRead((prev) => [...prev, book]);
       }
     });
   };
 
-  render() {
-    const { read, currentlyReading, wantToRead } = this.state;
-    return (
-      <div className="app">
-        <Switch>
-          <Route
-            exact
-            path="/search"
-            render={() => (
-              <div className="search-books">
-                <Search
-                  changeShelf={this.changeShelf}
-                  read={read}
-                  currentlyReading={currentlyReading}
-                  wantToRead={wantToRead}
+  return (
+    <div className="app">
+      <Switch>
+        <Route
+          exact
+          path="/search"
+          render={() => (
+            <div className="search-books">
+              <Search
+                changeShelf={changeShelf}
+                read={read}
+                currentlyReading={currentlyReading}
+                wantToRead={wantToRead}
+                searchTerm={searchTerm}
+                setSearchTerm={setSearchTerm}
+                debouncedSearchTerm={debouncedSearchTerm}
+              />
+            </div>
+          )}
+        />
+
+        <Route
+          path="/"
+          render={() => (
+            <div className="list-books">
+              <div className="list-books-title">
+                <h1>MyReads</h1>
+              </div>
+
+              <div className="list-books-content">
+                <BookShelf
+                  shelfName="Currently Reading"
+                  books={currentlyReading}
+                  changeShelf={changeShelf}
+                />
+
+                <BookShelf
+                  shelfName="Read"
+                  books={read}
+                  changeShelf={changeShelf}
+                />
+
+                <BookShelf
+                  shelfName="Want To Read"
+                  books={wantToRead}
+                  changeShelf={changeShelf}
                 />
               </div>
-            )}
-          />
-          <Route
-            path="/"
-            render={() => (
-              <div className="list-books">
-                <div className="list-books-title">
-                  <h1>MyReads</h1>
-                </div>
-                <div className="list-books-content">
-                  <BookShelf
-                    shelfName={"currently reading"}
-                    books={currentlyReading}
-                    changeShelf={this.changeShelf}
-                  />
-                  <BookShelf
-                    shelfName={"Read"}
-                    books={read}
-                    changeShelf={this.changeShelf}
-                  />
-                  <BookShelf
-                    shelfName={"Want to Read"}
-                    books={wantToRead}
-                    changeShelf={this.changeShelf}
-                  />
-                </div>
-                <div className="open-search">
-                  <Link to="/search">Add a book</Link>
-                </div>
+
+              <div className="open-search">
+                <Link to="/search">Add a book</Link>
               </div>
-            )}
-          />
-        </Switch>
-      </div>
-    );
-  }
+            </div>
+          )}
+        />
+      </Switch>
+    </div>
+  );
 }
+
 export default BooksApp;
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,108 +1,85 @@
-import React, { Component } from "react";
-import { withRouter, Link } from "react-router-dom";
-import PropTypes from "prop-types";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import * as BooksAPI from "../BooksAPI";
 import Book from "./Book";
 
-class Search extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      query: "",
-      searchResult: [],
-      searchError: false,
-    };
-  }
-  static propTypes = {
-    changeShelf: PropTypes.func.isRequired,
-    read: PropTypes.array.isRequired,
-    currentlyReading: PropTypes.array.isRequired,
-    wantToRead: PropTypes.array.isRequired,
-  };
-  checkBookExist = (books) => {
-    const { read, currentlyReading, wantToRead } = this.props;
-    const mainPageBooks = [...read, ...currentlyReading, ...wantToRead];
-    if (books.length > 0) {
-      for (const book of mainPageBooks) {
-        books
-          .filter((b) => b.id === book.id)
-          .map((b) => (b.shelf = book.shelf));
-      }
+function Search({
+  changeShelf,
+  read,
+  currentlyReading,
+  wantToRead,
+  searchTerm,
+  setSearchTerm,
+  debouncedSearchTerm,
+}) {
+  const [searchResult, setSearchResult] = useState([]);
+  const [searchError, setSearchError] = useState(false);
 
-      this.setState({
-        searchResult: books,
-        searchError: false,
+  const checkBookExist = (books) => {
+    const mainPageBooks = [...read, ...currentlyReading, ...wantToRead];
+
+    if (books && books.length > 0) {
+      const updatedBooks = books.map((b) => {
+        const exist = mainPageBooks.find((mb) => mb.id === b.id);
+        return exist ? { ...b, shelf: exist.shelf } : b;
       });
+
+      setSearchResult(updatedBooks);
+      setSearchError(false);
     } else {
-      this.setState({
-        searchResult: [],
-        searchError: true,
-      });
+      setSearchResult([]);
+      setSearchError(true);
     }
   };
 
-  handleInputSearch = () => {
-    const inputSearch = document.getElementById("inputSearch").value;
-    this.setState(() => ({
-      query: inputSearch.trim(),
-    }));
-    this.setState({ query: inputSearch });
-    if (inputSearch !== "") this.searchBook(inputSearch);
-  };
+  // 👈 هنا السحر بتاع debounce
+  useEffect(() => {
+    if (debouncedSearchTerm) {
+      BooksAPI.search(debouncedSearchTerm, 20).then((books) => {
+        checkBookExist(books);
+      });
+    } else {
+      setSearchResult([]);
+    }
+  }, [debouncedSearchTerm]);
 
-  searchBook(inputSearch) {
-    const { searchResult } = this.state;
-    BooksAPI.search(inputSearch, 20).then((books) => {
-      this.checkBookExist(books);
-      if (this.state.query === "")
-        this.setState({
-          searchResult: [],
-        });
-      return searchResult;
-    });
-  }
+  return (
+    <div className="search-books">
+      <div className="search-books-bar">
+        <Link to="/" className="close-search">
+          close
+        </Link>
 
-  render() {
-    const { query, searchResult, searchError } = this.state;
-    const { changeShelf } = this.props;
-    return (
-      <div className="search-books">
-        <div className="search-books-bar">
-          <Link to="/" className="close-search">
-            close
-          </Link>
-          <div className="search-books-input-wrapper">
-            <input
-              type="text"
-              placeholder="Search by title or author"
-              id="inputSearch"
-              onChange={this.handleInputSearch}
-            />
-          </div>
-        </div>
-        <div className="search-books-results">
-          {searchResult.length > 0 && query !== "" ? (
-            <ol className="books-grid">
-              {searchResult.map((book) => (
-                <Book
-                  book={book}
-                  key={book.id}
-                  changeShelf={changeShelf}
-                  shelf={book.shelf}
-                />
-              ))}
-            </ol>
-          ) : searchError === true && query !== "" ? (
-            <p id="noSearch">
-              Sorry!,there is no result for your enterd words:(
-            </p>
-          ) : (
-            <p id="noSearch">Search Result Will Be Shown Here</p>
-          )}
+        <div className="search-books-input-wrapper">
+          <input
+            type="text"
+            placeholder="Search by title or author"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
         </div>
       </div>
-    );
-  }
+
+      <div className="search-books-results">
+        {searchResult.length > 0 ? (
+          <ol className="books-grid">
+            {searchResult.map((book) => (
+              <Book
+                key={book.id}
+                book={book}
+                changeShelf={changeShelf}
+                shelf={book.shelf}
+              />
+            ))}
+          </ol>
+        ) : searchError && searchTerm !== "" ? (
+          <p>Sorry!, no results found 😢</p>
+        ) : (
+          <p>Search Result Will Be Shown Here</p>
+        )}
+      </div>
+    </div>
+  );
 }
 
-export default withRouter(Search);
+export default Search;
